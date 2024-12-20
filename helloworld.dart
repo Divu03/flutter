2221628fb17171a3d320d713cc1862460d13c190
Divu <divyeshkuchhadia@gmail.com>
2024-12-20 21:34:25 +0530

    learing variables in dart

--- helloworld.dart
+++ helloworld.dart
@@ -1,3 +1,14 @@
 void main(){
-  print("Hello World");
+
+var name = "Divyesh";
+String aname = "jay";
+String? bname;
+late String cname;
+
+cname = "vijay";
+bname = "ajay";
+
+  print(name + '\n'+aname + '\n'+cname);
+  print(bname);
+
 }
